884544a5ae26965d253aeb6f27128bbd7abce8e4
Chawa1 <chawankhalil11@gmail.com>
2022-09-07 23:31:05 +0300

    config: add tailwind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-red-400">
+    <h1 className="text-3xl font-bold bg-red-400 ">
     Hello world!
   </h1>
 
