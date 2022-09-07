aa38b0db4d75d7c66fd516014b4ba266d5ec2f64
Chawa1 <chawankhalil11@gmail.com>
2022-09-08 00:27:33 +0300

    feat: add dynamic route  for each product

--- src/App.js
+++ src/App.js
@@ -1,61 +1,31 @@
 import React from 'react';
-/* import logo from './logo.svg';
-import { Counter } from './features/counter/Counter'; */
+
+import { Routes, Route } from 'react-router-dom';  /* add dyanamic routing */
+
 import './App.css';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Cart from './pages/Cart';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Products from './pages/Products';
+import Product from './pages/Product';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold bg-red-400 ">
-    Hello world!
-  </h1>
-
-     /* {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header> */
-  
-  );
-}
+    <div>
+<Routes>  
+<Route path="/" element={<Home />}/>
+<Route path="about" element={<About />}/>
+<Route path="cart" element={<Cart />}/>
+<Route path="login" element={<Login />}/>
+<Route path="register" element={<Register />}/>
+<Route path="products/:id" element={<Products />}/>  {/* to crate dynamic api for each single product */}
+<Route path="product" element={<Product />}/>
+</Routes>
+    </div>
+    );
+    }
 
 export default App;
