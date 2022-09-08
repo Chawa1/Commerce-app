0c7826ae886683ddea9f6db95012925cbdc30478
Chawa1 <chawankhalil11@gmail.com>
2022-09-09 02:58:57 +0300

    feat: add Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,42 @@
 import React from 'react'
 
-const Login = () => {
+export default function Login() {
   return (
-    <div>Login</div>
+/* basic form  have: Title, Email/userName, Password, Submit Button */
+
+
+    <div className='bg-indigo-600 min-h-screen p-10 flex items-center justify-center'>
+
+      <div className='container mx-auto flex items-center justify-center'>
+
+    <div className='bg-white p-10 rounded-lg shadow-xl w-96'>
+      <form className='grid grid-cols-1'>
+      <h2 className='text-center mb-5 text-2xl font-bold'> Login Page </h2>
+
+      <label
+      htmlFor="username"> UserName</label> {/* htmlFor="username" boi ka click man la label aka krd input aka active bet */}
+      <input className='bg-gray-100 rounded-md mb-3 '
+       type="text" name="username" id="username" />
+       
+      <label htmlFor='password'>Password</label>
+      <input className='bg-gray-100 rounded-md '
+      type="text" name="password" id="password" />
+      <br/>
+
+      <button className="bg-indigo-900 p-1 rounded-full  text-white px-4  hover:bg-indigo-500 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800 hover:bg-indigo-500">
+        Login</button>
+
+        <div className='mt-5 text-sm text-gray-500 '>
+      <button className=' mt-1 underline'>Forget Password</button> {''}
+      <br/>
+      <button className=' mt-1 underline'>Don't have account register</button>
+      </div>
+
+      </form>
+    </div>
+    </div>
+    
+    </div>
   )
 }
 
-export default Login
