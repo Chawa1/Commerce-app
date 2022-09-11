2280ba2c38ab5751609ee9b6d25d837ed0c7f12e
Chawa1 <chawankhalil11@gmail.com>
2022-09-11 22:19:06 +0300

    style: fix layout styling

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 
 export default function ProductCard({ product }) {
   return ( 
-  <Link to={`/products/${product.id}`} className='rounded-lg bg-white p-4 '>  
+  <Link to={`/products/${product.id}`} className='rounded-lg shadow-md hover:shadow-xl transition-all  bg-white p-4  '>  {/* rounded-lg boi swchi cartaka bar axiri screen akaa nakawet */}
   
   <img 
   src={product.image} 
@@ -17,7 +17,7 @@ export default function ProductCard({ product }) {
 
   {/* ama bo bashi xwaraway cartakaya price w jorakai */}
   <div className='flex justify-between mt-8'>
-<p className='text-xl text-green-700 '>{product.price}$</p>
+<p className='text-2xl text-indigo-700 font-semibold '>{product.price}$</p>
 
 <button className='border border-indigo-600 rounded-xl text-indigo-600 text-xs p-1'>{product.category.toUpperCase()}</button>
 
--- src/components/ProductsList.js
+++ src/components/ProductsList.js
@@ -5,7 +5,7 @@ export default function ProductsList({ products }) {
   return (
    
     //<div className="container bg-slate-300 mx-auto p-10 grid grid-cols-1 sm:grid-cols-2  lg:grid-cols-3 xl:grid-cols-4  gap-10 object-fill">
-    <div className='container bg-slate-300 mx-auto grid grid-cols-12 gap-10'  > {/* gap space ii newan row w column akana */}
+    <div className='container mx-auto p-10 grid grid-cols-1  sm:grid-cols-2  lg:grid-cols-3  xl:grid-cols-4  gap-10 object-fill'  > {/* gap space ii newan row w column akana */}
     {products.map((product, index) => {
       return <ProductCard product={product}  key={index} />;
     })}
