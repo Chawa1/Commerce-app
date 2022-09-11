71abf93e4faf93d3a4aab19dedc0fc1cde1939d0
Chawa1 <chawankhalil11@gmail.com>
2022-09-11 14:24:40 +0300

    feat: add delimiter and productCard List

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { delimiter } from '../utils/delimiter';
+import {Link} from 'react-router-dom';
 
 export default function ProductCard({ product }) {
   return ( 
-  <div className='col-span-3 bg-white p-6 '>  
+  <Link to={'/products/${product.id}'} className='col-span-3 bg-white p-4 '>  
   
   <img 
   src={product.image} 
   alt='prodduct ' 
   className='h-60 w-full object-contain'/> {/* object-contain boi product aka hamwi darkawet */}
-  <h2 className='text-xl font-medium mt-6'> {product.title}</h2>
-  <p className='text-base text-gray-500'> {product.description}</p>
+  <h2 className='text-l font-medium mt-6 mb-6'> {delimiter(product.title, 30)}</h2>
+  <p className='text-base text-gray-500  '> 
+  {delimiter(product.description, 60)}
+  </p>
+
+  {/* ama bo bashi xwaraway cartakaya price w jorakai */}
+  <div className='flex justify-between mt-8'>
+<p className='text-xl text-green-700 '>40$</p>
+
+<button className='border border-indigo-600 rounded-xl text-indigo-600 text-xs p-1'>{product.category.toUpperCase()}</button>
+
   </div>
+  </Link>
   );
 }
