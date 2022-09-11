dc7d25cf51038566b0d9555e36ca8c18e7d71d5b
Chawa1 <chawankhalil11@gmail.com>
2022-09-11 15:58:31 +0300

    feat: add single product design

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import LoadingSpinner from '../components/LoadingSpinner';
 
+
 export default function Product() {
   let { id } = useParams();
 
@@ -29,6 +30,32 @@ export default function Product() {
 
   }
 
-  return <div> {JSON.stringify(product)}</div> ; 
+  return(
+     <div className='containet mx-auto grid grid-cols-2 divide-x-2  border-red-500 bg-white py-10 px-10'>
+
+
+<div className='' > {/* left side */}
+<h2 className='text-l font-medium mt-6 mb-6'> {product.title}</h2>
+  <p className='text-base text-gray-500  '> 
+  {product.description}
+  </p>
+
+  {/* ama bo bashi xwaraway cartakaya price w jorakai */}
+  <div className='flex justify-between mt-8'>
+<p className='text-xl text-green-700 '>40$</p>
+
+<button className='border border-indigo-600 rounded-xl text-indigo-600 text-xs p-1'>
+  {product.category.toUpperCase()}
+  </button>
+  </div>
+</div>
+
+<div className=''>
+  <img  src={product.image} alt='product'/> {/* right side include image */}
+  </div>
+
+     </div> 
+     
+  );
   
 }
