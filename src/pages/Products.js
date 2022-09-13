5799393833d65f675981cda6a5614bc36a36559b
Chawa1 <chawankhalil11@gmail.com>
2022-09-13 23:33:24 +0300

    feat: add pagination to products

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -16,6 +16,54 @@ export default function Products( ) {
   const [isLoading, setIsLoading] = useState(true);
 
 
+  //we start with an empty list of items pagination 
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(4); // 4 product la har page ka
+
+
+  //bo style pagination
+  const nextButton = (
+    //<button className="relative z-10 inline-flex items-center border border-indigo-500 bg-indigo-50 px-4 py-2 text-sm font-medium text-indigo-600 focus:z-20">
+    <button className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+       <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M14 5l7 7m0 0l-7 7m7-7H3"
+        />
+      </svg> 
+    </button>
+  );
+  const prevButton = (
+    <button className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        stroke-width="2"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M10 19l-7-7m0 0l7-7m-7 7h18"
+        />
+      </svg>
+    </button>
+  )
+
+
+
 
   useEffect(() => {  //categories akan la api aka waragrin 
     axios
@@ -30,7 +78,35 @@ export default function Products( ) {
       .catch((err) => {
         console.log(err);
       });
+
+//calculation for the react paginate, lerada zhmarai page akana
+
+if(products){
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  setCurrentItems(products.slice(itemOffset, endOffset));
+  setPageCount(Math.ceil(products.length / itemsPerPage)); 
+}
   }, []);
+
+  useEffect(()=> {
+    //calculation for the react paginate, lerada zhmarai page akana
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  setCurrentItems(products.slice(itemOffset, endOffset));
+  setPageCount(Math.ceil(products.length / itemsPerPage)); 
+
+  },[itemOffset, itemsPerPage, products]);
+
+  // Invoke when user click to request another page in paginate., lera ka page akan gora zhmarakan bgoren
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % products.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
+    );
+    setItemOffset(newOffset);
+  };
+
 
   if(isLoading){
     <LoadingSpinner/>
@@ -41,9 +117,22 @@ export default function Products( ) {
     <MainLayout>
     {/*   {JSON.stringify(products)}  */}
 
-    <ProductsList products = {products}/>
+    <ProductsList products = {currentItems}/> {/* ama item akaya */}
+    <div className="container mx-auto flex justify-center mb-10">
+        <ReactPaginate
+          breakLabel="..."
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          className="flex"
+          previousLabel={prevButton}
+          nextLabel={nextButton}
+          pageClassName="bg-white border-gray-300 text-gray-500  hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+          activeClassName="bg-indigo-300 border-gray-300 text-gray-500  hidden md:inline-flex relative items-center px-4 py-2 border text-sm font-medium"
+          renderOnZeroPageCount={null}
+        />
+      </div>
 
-    {/* add paginate component */}
     </MainLayout>
 
   )
